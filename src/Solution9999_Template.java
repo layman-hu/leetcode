3a0492f19e8f1f2cfb0834516392e3e9db50c0f4
layman-hu <1430291862@qq.com>
2022-01-19 22:23:25 +0800

    测试样例

--- src/Solution9999_Template.java
+++ src/Solution9999_Template.java
@@ -3,6 +3,7 @@ public class Solution9999_Template {
     //x.xxx
 
     public static void main(String[] args){
-
+        Solution9999_Template solution = new Solution9999_Template();
+        System.out.println();
     }
 }
