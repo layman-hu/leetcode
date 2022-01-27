23479a0228f874ef3ba938ef93caa9497b1e9563
layman-hu <1430291862@qq.com>
2022-01-27 18:48:12 +0800

    leetcode0543 二叉树的直径 二叉树的直径长度 等价于 1.寻找某一个节点，这个节点的左子树深度+右子树深度 2.在若干节点中，筛选出最大的值即可

--- src/TreeNode/Solution0543.java
+++ src/TreeNode/Solution0543.java
@@ -15,40 +15,64 @@ public class Solution0543 {
             this.right = right;
         }
     }
-    int result = 0;
-    int count = 0;//打印递归的计数器
+    int depth = 0;
+    int count = -1;//打印递归的计数器
+    int max_diameter = 0;//二叉树最大直径
 
-    public void printRecursion(int val, int depth, int count){
+    public void printRecursion(int node_val, int depth, int count){
         for(int i=0; i<count; i++){
             System.out.print("\t");
         }
-        System.out.println("val:"+val+" depth:"+depth);
+        System.out.println("val:"+node_val+" depth:"+depth);
     }
 
     //543. 二叉树的直径
-    public int diameterOfBinaryTree(TreeNode root) {
+    /*
+    二叉树的直径长度
+    等价于
+    1.寻找某一个节点，这个节点的左子树深度+右子树深度
+    2.在若干节点中，筛选出最大的值即可
+     */
+    public int maxDepth(TreeNode root) {
         if(root == null)return 0;
         count++;
-        printRecursion(root.val,result,count);
+        printRecursion(root.val,depth,count);
 
-        int left_depth = diameterOfBinaryTree(root.left);
-        int right_depth = diameterOfBinaryTree(root.right);
+        int left_depth = maxDepth(root.left);
+        int right_depth = maxDepth(root.right);
         //后序遍历
-        result = (left_depth>right_depth)?left_depth+1:right_depth+1;
 
-        printRecursion(root.val,result,count);
+        int temp_diameter = left_depth + right_depth;
+        max_diameter = max_diameter>temp_diameter?max_diameter:temp_diameter;
+
+        depth = (left_depth>right_depth)?left_depth+1:right_depth+1;
+
+        printRecursion(root.val,depth,count);
         count--;
-        return result;
+        return depth;
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+//        System.out.println(max_diameter);
+
+        return max_diameter;
     }
 
 
     public static void main(String[] args){
         Solution0543 solution = new Solution0543();
+
+
         TreeNode node1 = new TreeNode(1);
         TreeNode node2 = new TreeNode(2);
         TreeNode node3 = new TreeNode(3);
         TreeNode node4 = new TreeNode(4);
         TreeNode node5 = new TreeNode(5);
+
+        TreeNode node9 = new TreeNode(9);
+        node9.left = null;
+        node9.right = node1;
 
         node1.left = node2;
         node1.right = node3;
@@ -58,6 +82,7 @@ public class Solution0543 {
         node3.right = null;
 
 
-        System.out.println(solution.diameterOfBinaryTree(node1));
+//        System.out.println(solution.diameterOfBinaryTree(node1));
+        System.out.println(solution.diameterOfBinaryTree(node9));
     }
 }
